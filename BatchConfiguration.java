89b34e5c7b733f8892b2428682a66720a6a05667
eswaribala <Parameswaribala@gmail.com>
2018-03-11 18:01:15 +0530

    Add files via upload

--- BatchConfiguration.java
+++ BatchConfiguration.java
@@ -1,186 +1,133 @@
-package ch.javaee.springBootBatch;
+package com.quantvalley.batch;
 
-import ch.javaee.springBootBatch.model.Person;
-import ch.javaee.springBootBatch.processor.PersonItemProcessor;
-import ch.javaee.springBootBatch.tokenizer.PersonFixedLengthTokenizer;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JpaItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 
-import javax.sql.DataSource;
-import java.util.Properties;
+import com.quantvalley.batch.listener.JobCompletionNotificationListener;
+import com.quantvalley.batch.model.FxMarketEvent;
+import com.quantvalley.batch.model.FxMarketVolumeStore;
+import com.quantvalley.batch.model.Trade;
+import com.quantvalley.batch.processor.FxMarketEventProcessor;
+import com.quantvalley.batch.reader.FxMarketEventReader;
+import com.quantvalley.batch.reader.ReaderTasklet;
+import com.quantvalley.batch.writer.StockVolumeAggregator;
 
-
+/**
+ * The Class BatchConfiguration.
+ * 
+ * @author 
+ */
 @Configuration
 @EnableBatchProcessing
-@ComponentScan
-//spring boot configuration
-@EnableAutoConfiguration
-// file that contains the properties
-@PropertySource("classpath:application.properties")
 public class BatchConfiguration {
+	  
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
 
-    /*
-        Load the properties
-     */
-    @Value("${database.driver}")
-    private String databaseDriver;
-    @Value("${database.url}")
-    private String databaseUrl;
-    @Value("${database.username}")
-    private String databaseUsername;
-    @Value("${database.password}")
-    private String databasePassword;
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
 
+	@Bean
+	public FxMarketVolumeStore fxMarketPricesStore() {
+		return new FxMarketVolumeStore();
+	}
 
-    /**
-     * We define a bean that read each line of the input file.
-     *
-     * @return
-     */
-    @Bean
-    public ItemReader<Person> reader() {
-        // we read a flat file that will be used to fill a Person object
-        FlatFileItemReader<Person> reader = new FlatFileItemReader<Person>();
-        // we pass as parameter the flat file directory
-        reader.setResource(new ClassPathResource("PersonData.txt"));
-        // we use a default line mapper to assign the content of each line to the Person object
-        reader.setLineMapper(new DefaultLineMapper<Person>() {{
-            // we use a custom fixed line tokenizer
-            setLineTokenizer(new PersonFixedLengthTokenizer());
-            // as field mapper we use the name of the fields in the Person class
-            setFieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
-                // we create an object Person
-                setTargetType(Person.class);
-            }});
-        }});
-        return reader;
-    }
+	// FxMarketEventReader (Reader)
+	@Bean
+	public FxMarketEventReader fxMarketEventReader() {
+		return new FxMarketEventReader();
+	}
 
-    /**
-     * The ItemProcessor is called after a new line is read and it allows the developer
-     * to transform the data read
-     * In our example it simply return the original object
-     *
-     * @return
-     */
-    @Bean
-    public ItemProcessor<Person, Person> processor() {
-        return new PersonItemProcessor();
-    }
+	// FxMarketEventProcessor (Processor)
+	@Bean
+	public FxMarketEventProcessor fxMarketEventProcessor() {
+		return new FxMarketEventProcessor();
+	}
 
-    /**
-     * Nothing special here a simple JpaItemWriter
-     * @return
-     */
-    @Bean
-    public ItemWriter<Person> writer() {
-        JpaItemWriter writer = new JpaItemWriter<Person>();
-        writer.setEntityManagerFactory(entityManagerFactory().getObject());
+	// StockVolumeAggregator (Writer)
+	@Bean
+	public StockVolumeAggregator stockVolumeAggregator() {
+		return new StockVolumeAggregator();
+	}
 
-        return writer;
-    }
+	// JobCompletionNotificationListener (File loader)
+	@Bean
+	public JobExecutionListener listener() {
+		return new JobCompletionNotificationListener();
+	}
 
-    /**
-     * This method declare the steps that the batch has to follow
-     *
-     * @param jobs
-     * @param s1
-     * @return
-     */
-    @Bean
-    public Job importPerson(JobBuilderFactory jobs, Step s1) {
+	// Configure job step
+	@Bean
+	public Job fxMarketPricesETLJob() {
+		
+		FlowBuilder<Flow> flowBuilder = new FlowBuilder<Flow>("flow1");
 
-        return jobs.get("import")
-                .incrementer(new RunIdIncrementer()) // because a spring config bug, this incrementer is not really useful
-                .flow(s1)
-                .end()
-                .build();
-    }
+		  Flow flow =  flowBuilder
+		    .start(step1())
+		    .next(decision())
+		    .on(decision().COMPLETED)
+		    .to(step2())
+		    .from(decision())
+		    .on(decision().FAILED)
+		    .to(step3())
+		    .end();	
+		
+		
+		return jobBuilderFactory.get("FxMarket Volume ETL Job").incrementer(new RunIdIncrementer()).listener(listener())
+				.start(flow).end().build();
+	}
+	
+	@Bean
+	public TaskExecutor taskExecutor(){
+	    SimpleAsyncTaskExecutor asyncTaskExecutor=new SimpleAsyncTaskExecutor("spring_batch");
+	    asyncTaskExecutor.setConcurrencyLimit(5);
+	    return asyncTaskExecutor;
+	}
+    
+	@Bean
+	public Step step1() {
+		return stepBuilderFactory.get("Extract")
+				.tasklet(new ReaderTasklet())
+				//.processor(fxMarketEventProcessor())
+				
+				//.writer(stockVolumeAggregator())
+				.taskExecutor(taskExecutor()).build();
+	}
+	
+	@Bean
+	public Step step2() {
+		return stepBuilderFactory.get("Aggregate->load").<FxMarketEvent, Trade> chunk(10)
+				.processor(fxMarketEventProcessor())				
+				.writer(stockVolumeAggregator())
+				.taskExecutor(taskExecutor()).build();
+	}
 
-
-    /**
-     * Step
-     * We declare that every 1000 lines processed the data has to be committed
-     *
-     * @param stepBuilderFactory
-     * @param reader
-     * @param writer
-     * @param processor
-     * @return
-     */
-
-    @Bean
-    public Step step1(StepBuilderFactory stepBuilderFactory, ItemReader<Person> reader,
-                      ItemWriter<Person> writer, ItemProcessor<Person, Person> processor) {
-        return stepBuilderFactory.get("step1")
-                .<Person, Person>chunk(1000)
-                .reader(reader)
-                .processor(processor)
-                .writer(writer)
-                .build();
-    }
-
-    /**
-     * As data source we use an external database
-     *
-     * @return
-     */
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(databaseDriver);
-        dataSource.setUrl(databaseUrl);
-        dataSource.setUsername(databaseUsername);
-        dataSource.setPassword(databasePassword);
-        return dataSource;
-    }
-
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-
-        LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
-        lef.setPackagesToScan("ch.javaee.springBootBatch");
-        lef.setDataSource(dataSource());
-        lef.setJpaVendorAdapter(jpaVendorAdapter());
-        lef.setJpaProperties(new Properties());
-        return lef;
-    }
-
-
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
-        jpaVendorAdapter.setDatabase(Database.MYSQL);
-        jpaVendorAdapter.setGenerateDdl(true);
-        jpaVendorAdapter.setShowSql(false);
-
-        jpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
-        return jpaVendorAdapter;
-    }
-
+	@Bean
+	public Step step3() {
+		return stepBuilderFactory.get("Failure")
+				.tasklet(failureEventProcessor())
+				.build();
+	}
+	
+	public FailureTasklet failureEventProcessor()
+	{
+		return new FailureTasklet();
+	}
+	
+	 @Bean
+	 public FlowDecision decision(){
+	  return new FlowDecision();
+	 }
 }
